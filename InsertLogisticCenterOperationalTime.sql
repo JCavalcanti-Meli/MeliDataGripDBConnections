52fe8a1db33be45f9411e2a2fd2505d2df85378c
Luiz Antonio Rech <luiz.lrech@mercadolivre.com>
2021-08-13 17:08:17 -0300

    Add InsertLogisticCenterOperationalTime.sql

---
+++ InsertLogisticCenterOperationalTime.sql
@@ -0,0 +1,3 @@
+# Fulfillment  Logistic Center
+insert into logistic_center_operation_time (logistic_center_id, operation_start, expedition_start, expedition_end)
+values ('ARBA01','00:00:00','00:00:00','23:59:59');
